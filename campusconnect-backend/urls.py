31b54fb1cccef2f40afd28a55164bd9f1d68b266
Akshay Jadhav <akshayj2305@gmail.com>
2024-03-16 00:45:19 +0530

    second commit

--- campusconnect-backend/urls.py
+++ campusconnect-backend/urls.py
@@ -3,9 +3,13 @@ from django.contrib import admin
 from django.urls import path,include
 from home.views import *
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/',include('home.urls')),
     path('campusconnectpost/', register_campusconnect, name='register_student'),
     path('campusconnectdelete/<str:rollno>', delete_campusconnect, name='delete_campusconnect'),
+    # add at the last
+
 ]
+
